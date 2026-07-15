8463748be4f74ee044e4dcb11d307538738db99d
JuneAsz <eimantast003@gmail.com>
2026-07-15 22:57:10 +0300

    feat/normalized printing

--- src/print.rs
+++ src/print.rs
@@ -3,16 +3,15 @@ use colored::Colorize;
 pub fn print_palette(centroids: &[(u8, u8, u8)]) {
     println!("{}", "Extracted Palette".bold());
 
-    for (_, &(r, g, b)) in centroids.iter().enumerate() {
+    for &(r, g, b) in centroids {
+        let rgb = format!("RGB({:>3}, {:>3}, {:>3})", r, g, b);
         let hex = format!("#{:02X}{:02X}{:02X}", r, g, b);
 
         println!(
-            "{} {} {}",
-            format!("RGB({}, {}, {})", r, g, b)
-                .bold()
-                .truecolor(r, g, b),
+            "{:<21} {:<7} {}",
+            rgb.bold().truecolor(r, g, b),
             hex.bold(),
-            "████████".truecolor(r, g, b)
+            "████████".truecolor(r, g, b),
         );
     }
 }
