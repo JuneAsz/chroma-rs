32e57229bc179cdd1051fe4a6929a401e6ac39bc
JuneAsz <eimantast003@gmail.com>
2026-07-15 22:33:41 +0300

    feat/img resize before calc

--- src/img.rs
+++ src/img.rs
@@ -1,5 +1,11 @@
 use std::path::Path;
 
+use image::imageops;
+
 pub fn load_pixels(path: &Path) -> anyhow::Result<image::RgbImage> {
-    Ok(image::ImageReader::open(path)?.decode()?.into_rgb8())
+    let image = image::ImageReader::open(path)?.decode()?;
+
+    Ok(image
+        .resize(800, 800, imageops::FilterType::Triangle)
+        .into_rgb8())
 }
